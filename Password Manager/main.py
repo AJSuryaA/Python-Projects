5ba301d5e0a720d8e24a4b03bf62920d38d1b2a1
AJSuryaA <surya.2003amuari@gmail.com>
2024-12-14 15:03:20 +0530

    updated usage of json file and user warnings messagebox , Tkinter

--- Password Manager/main.py
+++ Password Manager/main.py
@@ -1,7 +1,6 @@
 import tkinter
 from tkinter import Tk, Canvas, PhotoImage, Label, Entry, Button , messagebox
-import pyperclip
-
+import pyperclip, json
 
 from generate_password import Generate_password
 
@@ -17,6 +16,7 @@ canvas.grid(column= 0,row=0, columnspan= 3)
 
 password= Generate_password()
 
+
 def generate():
     passw= password.generate()
     password_input.insert(index=0, string=f"{passw}")
@@ -30,19 +30,57 @@ def save_data():
         password_input.insert(index=0,string="Enter or generate password to continue")
         messagebox.askokcancel(title= "Warning", message= "enter all requisites to save")
     else:
+        web = website_input.get().title()
+        email = email_input.get()
+        p = password_input.get()
+        pass_dict = {
+            web: {
+                "email": email,
+                "password": p
+            }
+        }
         isok= messagebox.askokcancel(title="Warning", message=f"check the details you enter\n website :"
                             f"{website_input.get()} \n email : {email_input.get()} \n password : "
                                                               f"{password_input.get()} is it ok to save ")
         if isok:
-            p= password_input.get()
-            with open("data.txt", "a") as data_text:
-                data_text.write(f"{website_input.get()}|{email_input.get()}|{p}"+"\n")
-            website_input.delete(0,tkinter.END)
-            password_input.delete(0, tkinter.END)
-            oktocopy= messagebox.askokcancel(title="Warning", message="click ''ok'' to copy password to clipboard")
-            if oktocopy:
-                pyperclip.copy(p)
+            try:
+                with open(file="data.json", mode="r") as file:
+                    data= json.load(fp=file)
+            except FileNotFoundError:
+                with open(file= "data.json", mode="w") as file:
+                    json.dump(obj= pass_dict, fp= file, indent= 4)
+            else:
+                data.update(pass_dict)
+                with open(file= "data.json", mode="w") as file:
+                    json.dump(obj= data, fp= file, indent= 4)
+            finally:
+                website_input.delete(0,tkinter.END)
+                password_input.delete(0, tkinter.END)
+                oktocopy= messagebox.askokcancel(title="Warning", message="click ''ok'' to copy password to clipboard")
+                if oktocopy:
+                    pyperclip.copy(p)
 
+def search_password():
+    website_looking= website_input.get().title()
+    if website_looking != "":
+        try:
+            with open(file="data.json", mode="r") as file:
+                data= json.load(fp=file)
+        except FileNotFoundError:
+            messagebox.showinfo(title="Warning", message= "No Data File Found")
+        else:
+            if website_looking in data:
+                email= data[website_looking]["email"]
+                password1= data[website_looking]["password"]
+                askok= messagebox.askokcancel(title= "Search Result", message= f"email : {email} \n password : {password1}"
+                                                                     f"\nClick ''ok'' to copy password")
+                if askok:
+                    pyperclip.copy(password1)
+                    website_input.delete(0,tkinter.END)
+            else:
+                messagebox.showinfo(title="Warning", message= "No Data Found")
+    else:
+        messagebox.showinfo(title="Warning", message="Enter Website To Search")
 
 label_website= Label(text= "Website :", font= ("Arial ", 12 ,"bold"))
 label_website.grid(column= 0,row=1, padx= 20, pady= 5)
@@ -53,8 +91,8 @@ label_email.grid(column= 0,row=2, padx= 20, pady= 5)
 label_password= Label(text= "Password :", font= ("Arial ", 12 ,"bold"))
 label_password.grid(column= 0,row=3, padx= 20, pady= 5)
 
-website_input= Entry(width= 70 , highlightcolor= "Green" , highlightthickness= 2)
-website_input.grid(column= 1, row= 1, columnspan= 2)
+website_input= Entry(width= 30 , highlightcolor= "Green" , highlightthickness= 2)
+website_input.grid(column= 1, row= 1)
 
 email_input= Entry(width= 70 , highlightcolor= "Green" , highlightthickness= 2)
 email_input.grid(column= 1, row= 2, columnspan= 2)
@@ -68,5 +106,8 @@ add_button.grid(column= 1, row= 4, columnspan= 2, padx= 20, pady= 5)
 
 generate_button= Button(text= "Generate Password", width= 19, font= ("Arial ", 12 ,"bold"), command=generate)
 generate_button.grid(column= 2, row= 3, columnspan= 2, padx= 20, pady= 5)
+
+search_button= Button(text= "Search", width= 19, font= ("Arial ", 12 ,"bold"), command=search_password)
+search_button.grid(column= 2, row= 1, columnspan= 2,pady= 5)
 
 window.mainloop()
