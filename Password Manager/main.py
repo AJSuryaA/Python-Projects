e81028109120f668aaabe8eb05acba78784e1178
AJSuryaA <surya.2003amuari@gmail.com>
2024-12-14 11:36:18 +0530

    updated messagebox , Tkinter

--- Password Manager/main.py
+++ Password Manager/main.py
@@ -1,5 +1,8 @@
 import tkinter
-from tkinter import Tk, Canvas, PhotoImage, Label, Entry, Button
+from tkinter import Tk, Canvas, PhotoImage, Label, Entry, Button , messagebox
+
+from docutils.nodes import title
+
 from generate_password import Generate_password
 
 window= Tk()
@@ -22,13 +25,21 @@ def generate():
 def save_data():
     if website_input.get() == "":
         website_input.insert(index=0,string="Enter website to continue")
+        messagebox.askokcancel(title= "Warning", message= "enter all requisites to save")
+
     elif password_input.get() == "":
         password_input.insert(index=0,string="Enter or generate password to continue")
+        messagebox.askokcancel(title= "Warning", message= "enter all requisites to save")
     else:
-        with open("data.txt", "a") as data_text:
-            data_text.write(f"{website_input.get()}|{email_input.get()}|{password_input.get()}"+"\n")
-        website_input.delete(0,tkinter.END)
-        password_input.delete(0, tkinter.END)
+        isok= messagebox.askokcancel(title="Warning", message=f"check the details you enter\n website :"
+                            f"{website_input.get()} \n email : {email_input.get()} \n password : "
+                                                              f"{password_input.get()} is it ok to save ")
+        if isok:
+            with open("data.txt", "a") as data_text:
+                data_text.write(f"{website_input.get()}|{email_input.get()}|{password_input.get()}"+"\n")
+            website_input.delete(0,tkinter.END)
+            password_input.delete(0, tkinter.END)
+
 
 label_website= Label(text= "Website :", font= ("Arial ", 12 ,"bold"))
 label_website.grid(column= 0,row=1, padx= 20, pady= 5)
